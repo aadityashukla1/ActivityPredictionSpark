93978beceb69dfefb8bf903f421dd74c084bad6f
aadityashukla1 <aadityashukla@yahoo.com>
2016-12-04 17:46:01 -0500

    Update DeepNetActivity.R

--- DeepNetActivity.R
+++ DeepNetActivity.R
@@ -5,7 +5,7 @@ install.packages("splitstackshape")
 library(splitstackshape)
 
 #load the dataset into R 
-df <- read.csv("WISDM_at_v2.0_raw.txt", header = FALSE, stringsAsFactors = FALSE)
+df <- read.csv("WISDM_at_v2.0_raw.txt", header = FALSE)
 
 # view the dataset
 head(df,10)
@@ -21,18 +21,49 @@ rm(df.Sep)
 #now rename the dataframe columns
 colnames(df) <- c("user", "activity", "timestamp", "xaccel", "yaccel", "zaccel")
 
-df <- subset(df, select = -c(timestamp,user))
+df$timestamp <- as.numeric(df$timestamp)
+
+#df <- subset(df, select = -c(timestamp))
 table(df$activity)
 
 toBeRemoved<-which(df$activity=="")
 df<-df[-toBeRemoved,]
 df$activity <- factor(df$activity)
 
-# train the model
-#h2o.init()
 
-#h2o.shutdown()
-h2o.init(nthreads = -1)
+# now clean the data for autoencoder model
+mydat <- fread('https://s3.amazonaws.com/actidataset/WISDM_at_v2.0_unlabeled_raw.txt')
+
+detach("package:h2o", unload=TRUE)
+mydat$V3 <- as.numeric(mydat$V3)
+
+mydat <- subset(mydat, select = -c(V2))
+
+library(splitstackshape)
+
+mydat <- concat.split(data = mydat,split.col = "V6", sep = ";")
+mydat <- subset(mydat, select = -c(V6))
+colnames(mydat) <- c("user", "timestamp", "xaccel", "yaccel", "zaccel")
+
+# convert the dataframe to h2oframe
+h2o.init(nthreads=-1)
+mydat.hex <- as.h2o(mydat)
+
+AutEnc <- h2o.deeplearning(
+    model_id="dl_model_autoEncoder", 
+    training_frame=mydat.hex, 
+    x=predictors,
+    hidden=c(200,200,200,200,200),
+    epochs=500000,
+    activation = "Tanh",
+    autoencoder = TRUE,
+    loss = "Automatic",
+    stopping_metric = "AUTO"
+)
+
+h2o.saveModel(AutEnc, path = wd)
+
+# train deeplearning model
 
 df.hex <- as.h2o(df)
 
@@ -46,21 +77,22 @@ response <- "activity"
 predictors <- setdiff(names(df.hex), response)
 predictors
 
-
-modelActivity1 <- h2o.deeplearning(
+# not good model
+modelActivity <- h2o.deeplearning(
     model_id="dl_model_Activity", 
     training_frame=train, 
     validation_frame=valid,
     x=predictors,
     y=response,
-    hidden=c(200,200,200),                  ## small network, runs faster
-    epochs=100,                      ## hopefully converges earlier...
-    stopping_metric="misclassification", ## could be "MSE","logloss","r2"
-    stopping_tolerance=0.0000001
+    hidden=c(200,200,200,200,200),
+    epochs=10000,
+    stopping_metric="misclassification",
+    loss = "CrossEntropy",
+    activation = "Tanh",
 )
 summary(modelActivity1)
 
-
+# not good model
 modelActivity2 <- h2o.deeplearning(
     model_id="dl_model_Activity", 
     training_frame=train, 
@@ -74,3 +106,20 @@ modelActivity2 <- h2o.deeplearning(
     loss = "CrossEntropy"
 )
 summary(modelActivity2)
+
+# good one
+deepModelActi<- h2o.deeplearning(
+    model_id="dl_model", 
+    training_frame=train, 
+    validation_frame=valid,
+    x=predictors,
+    y=response,
+    hidden=c(200,200,200,200,200),
+    activation = "Tanh",
+    stopping_metric = "misclassification",
+    loss = "CrossEntropy",
+    stopping_rounds = 10,
+    epochs = 500000,
+    score_interval = 14,
+    pretrained_autoencoder ="dl_model_autoEncoder"
+)
